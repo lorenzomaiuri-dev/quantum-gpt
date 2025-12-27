2a9ec4409323558b0b0f46cadc6d49462c6be0a0
lorenzomaiuri-dev <maiurilorenzo@gmail.com>
2025-12-27 17:45:06 +0100

    added missing annotations

--- main.py
+++ main.py
@@ -35,8 +35,10 @@ def train(config, model_name, dataset_name):
     print(f"To view logs, run: tensorboard --logdir=experiments")
     
     # Save Configuration immediately
+    config_dict = asdict(config)
+    config_dict['n_qubits'] = config.n_qubits
     with open(os.path.join(run_dir, "config.json"), "w") as f:
-        json.dump(asdict(config), f, indent=4)
+        json.dump(asdict(config_dict), f, indent=4)
 
     # Dataset setup
     dataset_path = os.path.join("data", f"{dataset_name}")
--- src/config/default.py
+++ src/config/default.py
@@ -3,21 +3,21 @@ from dataclasses import dataclass
 
 @dataclass
 class GPTConfig:
-    batch_size = 16          # Number of independent sequences processed in parallel per training step
-    block_size = 32          # Maximum context length: the number of tokens the model can look back at
-    max_iters = 5000         # Total number of training iterations (steps)
-    eval_interval = 100      # How often (in iterations) to run the evaluation loop
-    learning_rate = 1e-3     # The step size used by the optimizer for weight updates
-    device = 'cuda' if torch.cuda.is_available() else 'cpu' # Hardware used for computation (GPU or CPU)
-    eval_iters = 200         # Number of batches to average over when estimating the loss during evaluation
-    n_embd = 64              # Total dimension of the token embeddings (hidden size)
-    n_head = 4               # Number of attention heads in the Multi-Head Attention mechanism
-    n_layer = 4              # Number of Transformer blocks (layers) in the model
-    dropout = 0.0            # Probability of dropping neurons during training to prevent overfitting
+    batch_size: int = 16          # Number of independent sequences processed in parallel per training step
+    block_size: int = 32          # Maximum context length: the number of tokens the model can look back at
+    max_iters: int = 5000         # Total number of training iterations (steps)
+    eval_interval: int = 100      # How often (in iterations) to run the evaluation loop
+    learning_rate: float = 1e-3   # The step size used by the optimizer for weight updates
+    device: str = 'cuda' if torch.cuda.is_available() else 'cpu' # Hardware used for computation (GPU or CPU)
+    eval_iters: int = 200         # Number of batches to average over when estimating the loss during evaluation
+    n_embd: int = 64              # Total dimension of the token embeddings (hidden size)
+    n_head: int = 4               # Number of attention heads in the Multi-Head Attention mechanism
+    n_layer: int = 4              # Number of Transformer blocks (layers) in the model
+    dropout = 0.0                 # Probability of dropping neurons during training to prevent overfitting
     
     # Quantum config
-    use_quantum: bool = False      # Toggle to enable or disable quantum-enhanced layers
-    n_qlayers: int = 2             # Depth (number of layers) of the variational quantum circuit
+    use_quantum: bool = False       # Toggle to enable or disable quantum-enhanced layers
+    n_qlayers: int = 2              # Depth (number of layers) of the variational quantum circuit
     q_device: str = 'default.qubit' # The backend simulator or quantum hardware used for execution
     
     @property
